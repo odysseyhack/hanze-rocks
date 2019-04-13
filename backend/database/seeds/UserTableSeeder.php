6f15ef6b7a21f3b84df0678c9543b58d51c5816a
Eran Machiels <e.machiels@hotmail.com>
2019-04-13 22:11:02 +0200

    Added seeder for test user

---
+++ backend/database/seeds/UserTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Test',
+            'email' => 'test@ebmdev.nl',
+            'password' => bcrypt('test123'),
+        ]);
+    }
+}
